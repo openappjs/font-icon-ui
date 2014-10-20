c85090f945b961a33a9c0bb1113b5d526af040dc
Simon Tegg <simon@loomio.org>
2014-10-20 17:51:57 +1300

    strip this back to avoid inserting css multiple times

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var mercury = require('mercury');
 var h = mercury.h;
-var RCSS = require('rcss');
-var xtend = require('xtend');
-var doubleQuote = require('to-double-quotes');
-var singleQoute = require('to-single-quotes');
 
 var screenReaderTextInlineStyle = {
   position: 'absolute',
@@ -11,13 +7,6 @@ var screenReaderTextInlineStyle = {
   left: '-9999px'
 };
 
-var fontCSS = {
-  speak: 'none',
-  textTransform: 'none',
-  "-webkit-font-smoothing": 'antialiased',
-  "-moz-osx-font-smoothing": 'grayscale',
-  textDecoration: 'none'
-};
 
 var FontIcon = function (options) {
   options = options || {};
@@ -38,18 +27,7 @@ var FontIcon = function (options) {
 FontIcon.render = function (state, events) {
   var model = state.model || {};
   var style = state.style || {};
-  var css = {};
-
-  // var unicode = model.unicode.valueOf("'") !== -1 ? model.unicode.
-
-  css[':before'] = xtend(
-    fontCSS, 
-    {fontFamily: model.fontFamily, content: model.unicode},
-    style);
-
-  var icon = RCSS.registerClass(css);
-
-  RCSS.injectAll();
+  var className = state.className
 
   return h('div.icon', { style: style.icon }, [
     h('span', {className: icon.className}),
